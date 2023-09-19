6002c14fcf9db5b53d64cb2eaa20964ef8bf8373
maju116 <michalmaj116@gmail.com>
2023-09-19 08:50:44 +0200

    Switch to RF

--- intro.py
+++ intro.py
@@ -3,18 +3,18 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
-# Initialize multiple experiments
-experiment1 = Experiment(api_key="hcUmYvDjaVusNZMhAoxq7sVDR",
-                        project_name="cometml-intro",
-                        workspace="maju116")
-experiment2 = Experiment(api_key="hcUmYvDjaVusNZMhAoxq7sVDR",
-                        project_name="cometml-intro",
-                        workspace="maju116")
 
 data = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2)
+
+# Experiment 1
+experiment1 = Experiment(api_key="hcUmYvDjaVusNZMhAoxq7sVDR",
+                        project_name="cometml-intro",
+                        workspace="maju116")
+experiment1.set_name("Logistic Regression Experiment")
+experiment1.add_tags(["LogisticRegression", "IrisDataset"])
 
 clf1 = LogisticRegression(max_iter=200)
 clf1.fit(X_train, y_train)
@@ -27,7 +27,14 @@ experiment1.log_metric("test_accuracy", test_accuracy1)
 
 experiment1.end()
 
-clf2 = DecisionTreeClassifier()
+# Experiment 2
+experiment2 = Experiment(api_key="hcUmYvDjaVusNZMhAoxq7sVDR",
+                        project_name="cometml-intro",
+                        workspace="maju116")
+experiment2.set_name("Random Forest Experiment")
+experiment2.add_tags(["RandomForest", "IrisDataset"])
+
+clf2 = RandomForestClassifier()
 clf2.fit(X_train, y_train)
 
 train_accuracy2 = clf2.score(X_train, y_train)
